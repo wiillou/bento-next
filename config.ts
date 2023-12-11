3e26969cc57aa6cfaaf81a6e0378dcb4f3ceeee8
wiillou <wiillou@proton.me>
2023-12-11 00:28:11 +0000

    fixed string issue with reddit

--- config.ts
+++ config.ts
@@ -186,7 +186,7 @@ export const config: Config = {
           },
           {
             name: 'Reddit',
-            url: 'https://reddit.com'',
+            url: 'https://reddit.com',
           },
           {
             name: 'Coolors',
